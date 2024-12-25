62004bcebf3ff9a6410766c34f84f03e403d67db
ajay-beast <ajaybro07072003@gmail.com>
2024-12-25 23:38:24 +0530

    added cart functionality

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ajay/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ajay/ecommerce/config/MyDataRestConfig.java
@@ -2,6 +2,10 @@ package com.ajay.ecommerce.config;
 
 import com.ajay.ecommerce.entity.Product;
 import com.ajay.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -11,6 +15,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+  private EntityManager entityManager;
+
+  @Autowired
+  public MyDataRestConfig(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
 
   @Override
   public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
@@ -26,5 +37,15 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
         .withCollectionExposure(
             ((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+    exposeIds(config);
+  }
+
+  private void exposeIds(RepositoryRestConfiguration config) {
+    Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+    config.exposeIdsFor(entities.stream()
+        .map(EntityType::getJavaType)
+        .toArray(Class[]::new));
+
   }
 }
