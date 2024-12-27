d0d029eda4173a63b003872f7f38720aae96a466
ajay-beast <ajaybro07072003@gmail.com>
2024-12-27 21:27:26 +0530

    Convert angular-ecommerce to a regular folder

---
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+
+  cartItems:CartItem[]=[];
+
+  totalPrice:Subject<number> = new Subject<number>;
+  totalQuantity:Subject<number> = new Subject<number>;
+
+  constructor() { }
+
+  addToCart(theCartItem:CartItem){
+    let alreadyExistsInCart:boolean=false;
+    let existingCartItem:CartItem | undefined=undefined;
+
+    if(this.cartItems.length > 0){
+      existingCartItem = this.cartItems.find(tempCartItem=>tempCartItem.id === theCartItem.id)
+      alreadyExistsInCart = (existingCartItem != undefined)
+    }
+
+    if(alreadyExistsInCart && existingCartItem) existingCartItem.quantity++;
+    else{
+      this.cartItems.push(theCartItem)
+    }
+
+    this.computeCartTotals();
+
+
+  }
+
+  decrementQuantity(cartItem:CartItem){
+    cartItem.quantity--;
+    if(cartItem.quantity === 0){
+      this.remove(cartItem);
+    }
+
+    else{
+      this.computeCartTotals();
+    }
+  }
+
+  remove(cartItem : CartItem){
+    const itemIndex = this.cartItems.findIndex(tempCartItem=> tempCartItem.id === cartItem.id);
+    if(itemIndex > -1){
+      this.cartItems.splice(itemIndex,1);
+      this.computeCartTotals();
+    }
+  }
+
+  computeCartTotals(){
+    let totalPriceValue:number=0;
+    let totalQuantityValue:number=0;
+
+    for(let currCartItem of this.cartItems){
+      totalPriceValue += currCartItem.unitPrice*currCartItem.quantity;
+      totalQuantityValue += currCartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+  }
+}
---
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,76 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+
+  private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
+
+  constructor(private httpClient : HttpClient) { }
+
+  getProductListPaginate(page: number, pageSize:number ,categoryId : number) : Observable<GetResponse> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`
+                      + `&page=${page}&size=${pageSize}`;
+    return this.httpClient.get<GetResponse>(searchUrl);
+  }
+
+  getProductList(categoryId : number) : Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+    return this.getProducts(searchUrl);
+  }
+
+  getProductCategories() : Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response=>response._embedded.productCategory)
+    );
+  }
+
+  searchProducts(keyword:string):Observable<Product[]>{
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${keyword}`;
+    return this.getProducts(searchUrl);
+  }
+
+
+  getSearchProductsPaginate(page: number, pageSize:number ,keyword : string) : Observable<GetResponse> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${keyword}`
+                      + `&page=${page}&size=${pageSize}`;
+    return this.httpClient.get<GetResponse>(searchUrl);
+  }
+
+  getProductDetails(productId:number) : Observable<Product>{
+    const producturl = `${this.baseUrl}/${productId}`;
+    return this.httpClient.get<Product>(producturl)
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse{
+  _embedded :{
+    products : Product[];
+  },
+  page:{
+    size:number,
+    totalElements:number,
+    totalPages:number,
+    number:number
+  }
+}
+
+interface GetResponseProductCategory{
+  _embedded :{
+    productCategory : ProductCategory[];
+  }
+}
